3e1905c7221b26d3ef84e87a4196448675c366ef
Fuseteam <fusekai@outlook.com>
2020-02-12 09:57:04 -0300

    reachitecture file structure fixed errors replaced mysql with mariadb

--- mysql_user.sh
+++ mysql_user.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Create MySQL user
 # Author : Kaushal Kishore <kaushal.rahuljaiswal@gmail.com>
+/usr/libexec/mariadb-prepare-db-dir mariadb.service > /dev/null 2>&1 &&
 /usr/bin/mysqld_safe > /dev/null 2>&1 &
 
 RET=1
@@ -11,6 +12,7 @@ while [[ RET -ne 0 ]]; do
     RET=$?
 done
 
+mysql -uroot -e "UPDATE mysql.user SET password=PASSWORD('${MYSQL_ROOT_PASS}') WHERE User='root'"
 mysql -uroot -e "CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASS}'"
 mysql -uroot -e "GRANT ALL PRIVILEGES ON *.* TO '${MYSQL_USER}'@'%' WITH GRANT OPTION"
 
